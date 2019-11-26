17bc914785da3d4f2c832fe4b91998991f72b200
VovaSidorov <VovaCivic@gmail.com>
2019-11-26 16:20:46 +0200

    added fields to variables

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let btnCalc = document.getElementById("start");
+
+let budVal = document.querySelector(".budget-value");
+let dayBudVal = document.querySelector(".daybudget-value");
+let levVal = document.querySelector(".level-value");
+let expVal = document.querySelector(".expenses-value");
+let optExpVal = document.querySelector(".optionalexpenses-value");
+let income  = document.querySelector(".income-value");
+let monSav = document.querySelector(".monthsavings-value");
+let yearSav = document.querySelector(".yearsavings-value");
+let tearVal = document.querySelector(".year-value");
+let monthVal = document.querySelector(".month-value");
+let dayVal = document.querySelector(".day-value");
+
+let expensesItemClass = document.querySelectorAll('.expenses-item');
+console.log(expensesItemClass);
+let btnApprove = document.querySelector('#expenses-item-btn');
+let btnCalculate = document.querySelector('#count-budget-btn');
+
+let optionalCostField = document.querySelectorAll(".optionalexpenses-item");
+console.log(optionalCostField);
+
+let chooseIncome = document.querySelector(".choose-income");
+let checkSavings = document.querySelector("#savings");
+let chooseSum = document.querySelector(".choose-sum");
+let choosePercent = document.querySelector(".choose-percent");
+let yearValue = document.querySelector(".year-value");
+let monthValue = document.querySelector(".monthValue");
+let dayValue = document.querySelector(".day-value");
